96786aacdb8b7c644c7e038bed88d01665f26b2b
Luke Moll <luke.moll99@gmail.com>
2018-08-22 13:14:40 +0100

    Added API docstrings

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from bottle import route, run, Bottle
+from bottle import Bottle, response
 from threading import Thread
 from operator import itemgetter
 import time
@@ -28,7 +28,7 @@ class API(Bottle):
         self.run(host=host, port=port)
 
     def get_user(self, user):
-        if user.startswith("I-"): # Not 100% sure what this means
+        if user.startswith("I-"): # IRC user
             return {
                 "name": user[2:].split(" ")[0], # Trim the (IRC) off IRC users
                 "id": user,
@@ -54,34 +54,75 @@ class API(Bottle):
     """ API endpoints """
 
     def index(self):
+        """ GET / """
+        response.content_type = "text/plain"
         return "Hello, two-bot!"
 
     def ids(self):
+        """ GET /ids 
+            application/json
+
+            Returns a JSON array of user IDs for all the users that two-bot 
+            tracks (any user that has been two'd). See GET /info/<user> for
+            more information on the user ID format.
+        """
+        # Bottle doesn't automatically JSONify lists like it does dicts
+        response.content_type = "application/json"
         return json.dumps(list(self.bot.twoinfo["twos"].keys()))
 
     def twos(self, user):
+        """ GET /twos/<user>
+            application/json
+
+            <user>: User ID from GET /ids. Either a Slack username or
+                    "I-<irc nick> (IRC)".
+            Returns the number of times ("twos") and the timestamp of the last
+            time ("last") a user was two'd.
+        """
         if user in self.bot.twoinfo["twos"]:
-            return json.dumps({
+            return {
                 "id": user,
                 "twos": self.bot.twoinfo["twos"][user],
                 "last": self.bot.twoinfo["lasttime"][user]
-            })
+            }
         self.abort(404, "No user with that ID") # Should we just fail with 0 instead?
 
     def leaderboard(self):
-        return json.dumps(
-            sorted([{
+        """ GET /leaderboard
+            application/json
+
+            Returns a sorted JSON array of objects. FIRST element is the user
+            with the MOST two's. Objects include:
+                id: user id as per GET /ids
+                name: Slack username or IRC nick (not unique)
+                twos: number of times two'd
+                last: timestamp of last two
+
+        """
+        response.content_type = "application/json"
+        return json.dumps(sorted([{
                 "id": user,
                 "name": self.get_user(user)["name"],
                 "twos": self.bot.twoinfo["twos"][user],
                 "last": self.bot.twoinfo["lasttime"][user]
                 }
                 for user in self.bot.twoinfo["twos"].keys()],
-            key=itemgetter("twos"), reverse=True)
-        )
+            key=itemgetter("twos"), reverse=True))
 
     def info(self, user):
-        return json.dumps(self.get_user(user))
+        """ GET /info/<user>
+            application/json
+
+            <user>: User ID from GET /ids. Either a Slack username or
+                    "I-<irc nick> (IRC)"
+            Returns information object about <user>:
+                id: <user> param as called
+                name: username or IRC nick
+                real_name: Real name from Slack or "<irc nick> (IRC)"
+                is_bot: True if either this user is an IRC user passed through
+                        the IRC bridge or a Slack bot that's been two'd (rare)
+        """
+        return self.get_user(user)
 
 def age(date):
     """ helper method to streamline """
